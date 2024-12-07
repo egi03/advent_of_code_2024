f62522f73910c9f7bde4f6fbdab6a559591863e0
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2024-12-07 20:16:51 +0100

    solved part 2

---
+++ day7/day7_p2.py
@@ -0,0 +1,41 @@
+from itertools import product 
+
+def calculate(equ, operats):
+    eq = equ.copy()
+    len_eq = len(eq)
+    opers_id = 0
+    total = 0
+
+    while len(eq) > 1:
+        if operats[opers_id] == '*':
+            eq[0] = eq[0] * eq[1] 
+        elif operats[opers_id] == '+': 
+            eq[0] = eq[0] + eq[1]
+        else:
+            eq[0] = int(str(eq[0]) + str(eq[1]))
+        opers_id += 1
+        eq.pop(1)
+
+    return eq[0]
+
+def try_eval(equation):
+    res, equ = equation.split(": ")
+    res = int(res)
+    num_of_operations = equ.count(" ")
+
+    equ = equ.split(' ')
+    equ = [int(x) for x in equ]
+
+    # all permutations with repeating 
+    possible_operations = [list(p) for p in product(['*','+','||'], repeat=num_of_operations)]
+
+    for op in possible_operations:
+        if calculate(equ, op) == res:
+            return res
+    return 0
+
+with open("input.txt") as f:
+    inp = f.read().splitlines()
+
+
+print(sum([try_eval(eq) for eq in inp]))
